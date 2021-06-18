278170b6cb2e768458cd10ce2818f7134b582612
oogioogi <detacl73@gmail.com>
2021-06-18 13:50:47 +0900

    -네비게이션 창이동및 색깔 변화

--- Sample/ContentView.swift
+++ Sample/ContentView.swift
@@ -12,26 +12,40 @@ struct ContentView: View {
     @State private var isTapped: Bool = false
     
     var body: some View {
-        VStack{
-            
-            HStack{
-                VstacksView()
-                VstacksView()
-                VstacksView()
-            }
-            .padding(isTapped ? 20.0 : 10.0)
-            .background(isTapped ? Color.blue : Color.red)
-            .onTapGesture {
-                withAnimation {
-                    self.isTapped.toggle()
+        
+        NavigationView{
+            VStack{
+                HStack{
+                    VstacksView()
+                    VstacksView()
+                    VstacksView()
+                } // Hstack
+                .padding(isTapped ? 20.0 : 10.0)
+                .background(isTapped ? Color.blue : Color.red)
+                .onTapGesture {
+                    withAnimation {
+                        self.isTapped.toggle()
+                    }
                 }
-            }
-            //.animation(/*@START_MENU_TOKEN@*/.easeIn/*@END_MENU_TOKEN@*/)
-            
-            Text("\(isTapped.description)")
-        }
+                //.animation(/*@START_MENU_TOKEN@*/.easeIn/*@END_MENU_TOKEN@*/)
+                
+                Text("\(isTapped.description)")
+                
+                NavigationLink(
+                    destination: MytextView(),
+                    label: {
+                        Text("네비게이션!")
+                            .bold()
+                            .padding()
+                            .background(Color.yellow)
+                            .foregroundColor(.black)
+                            .cornerRadius(15.0)
+                    })
+                    .padding()
 
-
+            } // Vstack
+        } //NavigationView
+        
     }
 }
 
