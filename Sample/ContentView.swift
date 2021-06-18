a71be96fc7ffc218dbd8f67a3f08a51c3c806ec1
oogioogi <detacl73@gmail.com>
2021-06-18 12:57:49 +0900

    -tap Gesture

--- Sample/ContentView.swift
+++ Sample/ContentView.swift
@@ -4,18 +4,33 @@
 //
 //  Created by 이용석 on 2021/06/18.
 //
-
+// 바인딩 전!
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var isTapped: Bool = false
+    
     var body: some View {
-        HStack{
-            VstacksView()
-            VstacksView()
-            VstacksView()
+        VStack{
+            
+            HStack{
+                VstacksView()
+                VstacksView()
+                VstacksView()
+            }
+            .padding(isTapped ? 20.0 : 10.0)
+            .background(isTapped ? Color.blue : Color.red)
+            .onTapGesture {
+                withAnimation {
+                    self.isTapped.toggle()
+                }
+            }
+            //.animation(/*@START_MENU_TOKEN@*/.easeIn/*@END_MENU_TOKEN@*/)
+            
+            Text("\(isTapped.description)")
         }
-        .padding()
-        .background(Color.blue)
+
 
     }
 }
